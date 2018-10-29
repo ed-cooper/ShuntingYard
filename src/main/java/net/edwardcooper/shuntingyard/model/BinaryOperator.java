2a5b391c9ec2fd21ce51121b44c8e717cb6f4267
Edward Cooper <edward.cooper100@gmail.com>
2018-10-29 20:44:08 +0000

    Add BinaryOperator fields

--- src/main/java/net/edwardcooper/shuntingyard/model/BinaryOperator.java
+++ src/main/java/net/edwardcooper/shuntingyard/model/BinaryOperator.java
@@ -1,4 +1,28 @@
 package net.edwardcooper.shuntingyard.model;
 
-public class BinaryOperator {
+import java.util.function.BiFunction;
+
+public class BinaryOperator extends Operator {
+    protected BiFunction<Double, Double, Double> action;
+    protected boolean leftAssociative;
+    protected int precedence;
+
+    public BinaryOperator(String name, int precedence, boolean leftAssociative, BiFunction<Double, Double, Double> action) {
+        this.name = name;
+        this.precedence = precedence;
+        this.leftAssociative = leftAssociative;
+        this.action = action;
+    }
+
+    public BiFunction<Double, Double, Double> getAction() {
+        return action;
+    }
+
+    public boolean getLeftAssociative() {
+        return leftAssociative;
+    }
+
+    public int getPrecedence() {
+        return precedence;
+    }
 }
