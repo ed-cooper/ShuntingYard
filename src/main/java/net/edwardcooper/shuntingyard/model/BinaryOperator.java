bd91352759a7b5dcf4fbb56dda0173f7fca9bf17
Edward Cooper <edward.cooper100@gmail.com>
2018-10-29 22:15:03 +0000

    Use inbuilt DoubleBinaryOperator and DoubleUnaryOperator classes

--- src/main/java/net/edwardcooper/shuntingyard/model/BinaryOperator.java
+++ src/main/java/net/edwardcooper/shuntingyard/model/BinaryOperator.java
@@ -1,13 +1,13 @@
 package net.edwardcooper.shuntingyard.model;
 
-import java.util.function.BiFunction;
+import java.util.function.DoubleBinaryOperator;
 
 public class BinaryOperator extends Operator {
-    private final BiFunction<Double, Double, Double> action;
+    private final DoubleBinaryOperator action;
     private final boolean leftAssociative;
     private final int precedence;
 
-    public BinaryOperator(String name, int precedence, boolean leftAssociative, BiFunction<Double, Double, Double> action) {
+    public BinaryOperator(String name, int precedence, boolean leftAssociative, DoubleBinaryOperator action) {
         super(name);
 
         this.precedence = precedence;
@@ -15,7 +15,7 @@ public class BinaryOperator extends Operator {
         this.action = action;
     }
 
-    public BiFunction<Double, Double, Double> getAction() {
+    public DoubleBinaryOperator getAction() {
         return action;
     }
 
--- src/main/java/net/edwardcooper/shuntingyard/model/UnaryOperator.java
+++ src/main/java/net/edwardcooper/shuntingyard/model/UnaryOperator.java
@@ -1,17 +1,17 @@
 package net.edwardcooper.shuntingyard.model;
 
-import java.util.function.Function;
+import java.util.function.DoubleUnaryOperator;
 
 public class UnaryOperator extends Operator {
-    private final Function<Double, Double> action;
+    private final DoubleUnaryOperator action;
 
-    public UnaryOperator(String name, Function<Double, Double> action) {
+    public UnaryOperator(String name, DoubleUnaryOperator action) {
         super(name);
 
         this.action = action;
     }
 
-    public Function<Double, Double> getAction() {
+    public DoubleUnaryOperator getAction() {
         return action;
     }
 }
