36bab852173d7611546d9231e3f1798fa6fe8ded
Edward Cooper <edward.cooper100@gmail.com>
2018-10-29 21:31:20 +0000

    Add field to UnaryOperator class

--- src/main/java/net/edwardcooper/shuntingyard/model/UnaryOperator.java
+++ src/main/java/net/edwardcooper/shuntingyard/model/UnaryOperator.java
@@ -1,4 +1,17 @@
 package net.edwardcooper.shuntingyard.model;
 
-public class UnaryOperator {
+import java.util.function.Function;
+
+public class UnaryOperator extends Operator {
+    private final Function<Double, Double> action;
+
+    public UnaryOperator(String name, Function<Double, Double> action) {
+        super(name);
+
+        this.action = action;
+    }
+
+    public Function<Double, Double> getAction() {
+        return action;
+    }
 }
