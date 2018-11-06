91af04d5e3991794542eb60c73dcac845718ba59
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 22:15:04 +0000

    Add getSupportedOutputTokenTypes to Lexer

--- src/main/java/net/edwardcooper/shuntingyard/lexers/DefaultLexer.java
+++ src/main/java/net/edwardcooper/shuntingyard/lexers/DefaultLexer.java
@@ -3,6 +3,8 @@ package net.edwardcooper.shuntingyard.lexers;
 import net.edwardcooper.shuntingyard.model.Token;
 import net.edwardcooper.shuntingyard.model.TokenNotRecognisedException;
 
+import java.lang.reflect.Type;
+
 /**
  * The default lexer for tokenising maths expressions.
  */
@@ -10,5 +12,10 @@ public class DefaultLexer extends LexerBase {
     @Override
     public Token readToken(String input, int start) throws TokenNotRecognisedException {
         throw new TokenNotRecognisedException(input, start);
+    }
+
+    @Override
+    public Type[] getSupportedOutputTokenTypes() {
+        return new Type[] {};
     }
 }
--- src/main/java/net/edwardcooper/shuntingyard/lexers/Lexer.java
+++ src/main/java/net/edwardcooper/shuntingyard/lexers/Lexer.java
@@ -2,6 +2,7 @@ package net.edwardcooper.shuntingyard.lexers;
 
 import net.edwardcooper.shuntingyard.model.Token;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -14,4 +15,10 @@ public interface Lexer {
      * @return              A list of <code>Token</code> objects representing the input string.
      */
     List<Token> readTokens(String input);
+
+    /**
+     * Gets the supported types of tokens that this lexer can output.
+     * @return              The supported types of tokens that this lexer can output.
+     */
+    Type[] getSupportedOutputTokenTypes();
 }
