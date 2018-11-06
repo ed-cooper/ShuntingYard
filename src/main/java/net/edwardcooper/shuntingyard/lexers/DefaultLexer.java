228d60dc7f02a4fdffe99a5a80fcab88c1caf986
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 20:19:51 +0000

    Begin default Lexer implementation

---
+++ src/main/java/net/edwardcooper/shuntingyard/lexers/DefaultLexer.java
@@ -0,0 +1,16 @@
+package net.edwardcooper.shuntingyard.lexers;
+
+import net.edwardcooper.shuntingyard.model.Token;
+import net.edwardcooper.shuntingyard.model.TokenNotRecognisedException;
+
+/**
+ * The default lexer for tokenising maths expressions.
+ */
+public class DefaultLexer extends LexerBase {
+
+    @Override
+    public Token ReadToken(String input, int start) throws TokenNotRecognisedException {
+        throw new TokenNotRecognisedException(input, start);
+    }
+
+}
