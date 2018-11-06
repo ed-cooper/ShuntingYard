a538b811da68b3160bda6bc77ddbc0144722a897
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 20:42:01 +0000

    Create UnaryOperatorToken

---
+++ src/main/java/net/edwardcooper/shuntingyard/model/UnaryOperatorToken.java
@@ -0,0 +1,29 @@
+package net.edwardcooper.shuntingyard.model;
+
+public class UnaryOperatorToken extends OperatorToken {
+    private UnaryOperator operation;
+
+    /**
+     * UnaryOperatorToken class constructor.
+     * @param literal       The string literal that this token represents.
+     * @param operation     The operation that this token corresponds to.
+     */
+    protected UnaryOperatorToken(String literal, UnaryOperator operation) {
+        super(literal);
+
+        this.operation = operation;
+    }
+
+    @Override
+    public Operator getOperation() {
+        return operation;
+    }
+
+    /**
+     * Gets the unary operation that this token corresponds to.
+     * @return              The unary operation that this token corresponds to.
+     */
+    public UnaryOperator getUnaryOperation() {
+        return operation;
+    }
+}
