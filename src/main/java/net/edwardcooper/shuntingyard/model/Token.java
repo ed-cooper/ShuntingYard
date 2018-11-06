8540a74591713ad57002e96b10d8b0d130c2f75a
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 20:18:54 +0000

    Implement base Token class

--- src/main/java/net/edwardcooper/shuntingyard/model/Token.java
+++ src/main/java/net/edwardcooper/shuntingyard/model/Token.java
@@ -1,4 +1,24 @@
 package net.edwardcooper.shuntingyard.model;
 
-public class Token {
+/**
+ * Represents a single element within an equation.
+ */
+public abstract class Token {
+    private String literal;
+
+    /**
+     * Token class constructor.
+     * @param literal       The string literal that this token represents.
+     */
+    protected Token(String literal) {
+        this.literal = literal;
+    }
+
+    /**
+     * Gets the string literal that this token represents.
+     * @return              The string literal that this token represents.
+     */
+    public String getLiteral() {
+        return literal;
+    }
 }
