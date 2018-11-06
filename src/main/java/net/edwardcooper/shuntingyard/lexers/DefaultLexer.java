253c21e87271b33e869ed72e89ed26bd6be15216
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 22:38:11 +0000

    Create default operator list

--- src/main/java/net/edwardcooper/shuntingyard/lexers/DefaultLexer.java
+++ src/main/java/net/edwardcooper/shuntingyard/lexers/DefaultLexer.java
@@ -1,14 +1,48 @@
 package net.edwardcooper.shuntingyard.lexers;
 
-import net.edwardcooper.shuntingyard.model.Token;
-import net.edwardcooper.shuntingyard.model.TokenNotRecognisedException;
+import net.edwardcooper.shuntingyard.model.*;
 
 import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Arrays;
 
 /**
  * The default lexer for tokenising maths expressions.
  */
 public class DefaultLexer extends LexerBase {
+    private List<Operator> operators = Arrays.asList(
+        new BinaryOperator("+", 2, true,  (x, y) -> x + y),
+        new BinaryOperator("-", 2, true,  (x, y) -> x - y),
+        new BinaryOperator("*", 3, true,  (x, y) -> x * y),
+        new BinaryOperator("×", 3, true,  (x, y) -> x * y),
+        new BinaryOperator("/", 3, true,  (x, y) -> x / y),
+        new BinaryOperator("÷", 3, true,  (x, y) -> x / y),
+        new BinaryOperator("^", 4, false, (x, y) -> Math.pow(x, y)),
+        new UnaryOperator("−",      (x) -> -x),
+        new UnaryOperator("sqrt",   (x) -> Math.sqrt(x)),
+        new UnaryOperator("√",      (x) -> Math.sqrt(x)),
+        new UnaryOperator("sinh",   (x) -> Math.sinh(x)),
+        new UnaryOperator("cosh",   (x) -> Math.cosh(x)),
+        new UnaryOperator("tanh",   (x) -> Math.tanh(x)),
+        new UnaryOperator("sin",    (x) -> Math.sin(x)),
+        new UnaryOperator("cos",    (x) -> Math.cos(x)),
+        new UnaryOperator("tan",    (x) -> Math.tan(x)),
+        new UnaryOperator("asinh",  (x) -> Math.log(x + Math.sqrt(x * x + 1))),
+        new UnaryOperator("acosh",  (x) -> Math.log(x + Math.sqrt(x * x - 1))),
+        new UnaryOperator("atanh",  (x) -> Math.log((1 + x) / (1 - x)) / 2),
+        new UnaryOperator("asin",   (x) -> Math.asin(x)),
+        new UnaryOperator("arcsin", (x) -> Math.asin(x)),
+        new UnaryOperator("acos",   (x) -> Math.acos(x)),
+        new UnaryOperator("arccos", (x) -> Math.acos(x)),
+        new UnaryOperator("atan",   (x) -> Math.atan(x)),
+        new UnaryOperator("arctan", (x) -> Math.atan(x)),
+        new UnaryOperator("log_10", (x) -> Math.log10(x)),
+        new UnaryOperator("log_2",  (x) -> Math.log(x) / Math.log(2)),
+        new UnaryOperator("ln",     (x) -> Math.log(x)),
+        new MultiOutputUnaryOperator("±", (x) -> Arrays.asList(x, -x)),
+        new MultiOutputUnaryOperator("∓", (x) -> Arrays.asList(-x, x))
+    );
+
     @Override
     public Token readToken(String input, int start) throws TokenNotRecognisedException {
         throw new TokenNotRecognisedException(input, start);
