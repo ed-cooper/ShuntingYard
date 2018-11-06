69bac34a501a02fda3c336ec4bec1cc54b485bbb
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 10:29:32 +0000

    Add Javadoc to Operator

--- src/main/java/net/edwardcooper/shuntingyard/model/Operator.java
+++ src/main/java/net/edwardcooper/shuntingyard/model/Operator.java
@@ -1,12 +1,23 @@
 package net.edwardcooper.shuntingyard.model;
 
+/**
+ * Represents an operator in a mathematical expression.
+ */
 public abstract class Operator {
     private final String name;
 
+    /**
+     * Operator class constructor.
+     * @param name  The name of the operator being defined.
+     */
     protected Operator(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the name of this Operator.
+     * @return      The name of the operator defined by this instance.
+     */
     public String getName() {
         return name;
     }
