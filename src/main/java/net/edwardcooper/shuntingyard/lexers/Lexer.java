8c10e058e20592ab6e1a66fbff74479c4e4de933
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 20:19:18 +0000

    Change Lexer.ReadTokens return type to List<Token>

--- src/main/java/net/edwardcooper/shuntingyard/lexers/Lexer.java
+++ src/main/java/net/edwardcooper/shuntingyard/lexers/Lexer.java
@@ -3,6 +3,8 @@ package net.edwardcooper.shuntingyard.lexers;
 import net.edwardcooper.shuntingyard.model.Token;
 import net.edwardcooper.shuntingyard.model.TokenNotRecognisedException;
 
+import java.util.List;
+
 /**
  * Provides methods to split an input string into a series of tokens.
  */
@@ -11,9 +13,9 @@ public interface Lexer {
     /**
      * Splits an input string into a series of tokens.
      * @param input         The input string to split.
-     * @return              An array of <code>Token</code> objects representing the input string.
+     * @return              A list of <code>Token</code> objects representing the input string.
      */
-    Token[] ReadTokens(String input);
+    List<Token> ReadTokens(String input);
 
     /**
      * Reads a single token at the specified location within an input string.
