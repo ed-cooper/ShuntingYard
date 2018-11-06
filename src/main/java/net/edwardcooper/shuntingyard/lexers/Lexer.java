208b86180b92b81071cc0abee9da8e34edeafcb9
Edward Cooper <edward.cooper100@gmail.com>
2018-11-06 16:28:17 +0000

    Convert Lexer to interface and add Javadoc

--- src/main/java/net/edwardcooper/shuntingyard/lexers/Lexer.java
+++ src/main/java/net/edwardcooper/shuntingyard/lexers/Lexer.java
@@ -1,9 +1,26 @@
 package net.edwardcooper.shuntingyard.lexers;
 
 import net.edwardcooper.shuntingyard.model.Token;
+import net.edwardcooper.shuntingyard.model.TokenNotRecognisedException;
 
-public abstract class Lexer {
-    public abstract Token[] ReadTokens(String input);
+/**
+ * Provides methods to split an input string into a series of tokens.
+ */
+public interface Lexer {
 
-    public abstract Token ReadToken(String input, int start);
+    /**
+     * Splits an input string into a series of tokens.
+     * @param input         The input string to split.
+     * @return              An array of <code>Token</code> objects representing the input string.
+     */
+    Token[] ReadTokens(String input);
+
+    /**
+     * Reads a single token at the specified location within an input string.
+     * @param input                         The input string to find a token within.
+     * @param start                         The position of the first character of the token.
+     * @return                              The token at the specified location within the input string.
+     * @throws TokenNotRecognisedException  Thrown when a token could not be recognised.
+     */
+    Token ReadToken(String input, int start) throws TokenNotRecognisedException;
 }
