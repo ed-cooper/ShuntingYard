8ead8f32c57dccb060d5b981a75dc2e0faa9adc5
Edward Cooper <edward.cooper100@gmail.com>
2018-11-08 23:47:40 +0000

    Create DefaultParser

---
+++ src/main/java/net/edwardcooper/shuntingyard/parsers/DefaultParser.java
@@ -0,0 +1,12 @@
+package net.edwardcooper.shuntingyard.parsers;
+
+import net.edwardcooper.shuntingyard.model.Token;
+
+import java.util.List;
+
+public class DefaultParser extends ParserBase {
+    @Override
+    public List<Token> parse(List<Token> equation) {
+        return null;
+    }
+}
