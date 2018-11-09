355267df688d4829d2836281061faf577cf28dc7
Edward Cooper <edward.cooper100@gmail.com>
2018-11-09 14:34:31 +0000

    Add cartesian product method

--- src/main/java/net/edwardcooper/shuntingyard/evaluators/EvaluatorBase.java
+++ src/main/java/net/edwardcooper/shuntingyard/evaluators/EvaluatorBase.java
@@ -1,4 +1,28 @@
 package net.edwardcooper.shuntingyard.evaluators;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class EvaluatorBase implements Evaluator {
+    /**
+     * Gets the cartesian product of 2 lists.
+     * @param set1              The first set of values.
+     * @param set2              The second set of values.
+     * @return                  The cartesian product of 2 lists.
+     */
+    protected List<Double[]> cartesian(List<Double> set1, List<Double> set2) {
+        // Create output
+        ArrayList<Double[]> output = new ArrayList<>();
+
+        // For each item from set 1
+        for (double n : set1) {
+            // For each item from set 2
+            for (double m : set2) {
+                // Add pair
+                output.add(new Double[] {m, n});
+            }
+        }
+
+        return output;
+    }
 }
